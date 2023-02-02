fb6deddf757f18545ef9687d07fece39145cd117
Igor Avdonin <thathorizon@gmail.com>
2023-02-02 12:19:13 +0300

    Add new example

---
+++ src/_6/PecsExample.java
@@ -0,0 +1,94 @@
+package _6;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class PecsExample {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        DebitAccount debitAccount = new DebitAccount();
+        CreditAccount creditAccount = new CreditAccount(123);
+
+        bankAccount = debitAccount; // ok
+
+    }
+
+    // зачем придумали дженерики
+    public void example0() {
+        ArrayList creaditAccounts = new ArrayList();
+        creaditAccounts.add(new CreditAccount(123));
+
+        // где-то в программе происходит добавление другого типа
+        creaditAccounts.add(new DebitAccount());
+
+        for (Object o : creaditAccounts) {
+            System.out.println(((CreditAccount)o).limit); // программа упадет с ошибкой на элементе с индексом 1
+        }
+    }
+
+    // инвариантность дженериков
+    public void example1(DebitAccount debitAccount, BankAccount bankAccount) {
+        Profile<DebitAccount> dp = new Profile<>( debitAccount );
+        Profile<BankAccount> bp = new Profile<>( bankAccount );
+
+        bp = dp; // не ок
+    }
+
+    // делаем инвариантный объект ковариантным (сохраняем иерархию наследования)
+    public void example2(DebitAccount debitAccount, BankAccount bankAccount) {
+        Profile<DebitAccount> dp = new Profile<>( debitAccount );
+        Profile<? extends BankAccount> bp = new Profile<>( bankAccount );
+
+        bp = dp;
+    }
+
+    // делаем метод безопасным с помощью ограничений
+    public void example3_copy(ArrayList<? extends BankAccount> src, ArrayList<? super BankAccount> dst) {
+        for (BankAccount account : src) {
+            dst.add(account);
+        }
+
+        // src.add(new BankAccount()); не можем добавлять объекты в src
+        // BankAccount account = dst.get(0); не можем доставать объекты из dst
+        // не можем перепутать списки и скопировать не оттуда не туда
+        // применения super, кроме как ограничений на чтение, не используются
+    }
+
+    // делаем список Profile
+    // когда может пригодиться ковариантность дженериков
+    public void example4(DebitAccount debitAccount, BankAccount bankAccount) {
+        Profile<DebitAccount> dp = new Profile<>( debitAccount );
+        Profile<BankAccount> bp = new Profile<>( bankAccount );
+
+        ArrayList<Profile<BankAccount>> profiles = new ArrayList<>();
+        profiles.add(bp);
+        profiles.add(dp); // инвариантность дженериков не дает нам это сделать
+
+
+        // делаем ковариантность (сохраняем иерархию наследования)
+        ArrayList<Profile<? extends BankAccount>> profilesExtended = new ArrayList<>();
+        profilesExtended.add(bp);
+        profilesExtended.add(dp);
+
+    }
+}
+
+class BankAccount {}
+
+class DebitAccount extends BankAccount {}
+
+class CreditAccount {
+    int limit;
+
+    CreditAccount(int limit) {
+        this.limit = limit;
+    }
+}
+
+class Profile<T> {
+    T account;
+
+    Profile(T account) {
+        this.account = account;
+    }
+}
